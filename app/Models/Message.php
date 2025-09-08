0206a23a48304058e24e79c718bc58b8bd835003
floriankipeta <floriankipeta1@gmail.com>
2025-09-08 19:38:01 +0300

    Add message type constants to `Message` model for improved type management.

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    const TEXT_MESSAGE = 'text';
+    const BUTTON_MESSAGE = 'button';
+    const IMAGE_MESSAGE = 'image';
+    const AUDIO_MESSAGE = 'audio';
+    const VIDEO_MESSAGE = 'video';
+    const DOCUMENT_MESSAGE = 'document';
+
     protected $fillable = ['message_id', 'customer_id', 'type', 'text', 'timestamp', 'status'];
 }
